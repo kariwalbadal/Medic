d0f0014717369f271b297379eb6be9b8a743c3fb
Badal Kariwal <kariwalbadal@gmail.com>
2018-03-19 19:01:08 +0530

    .

--- test.py
+++
@@ -1,9 +0,0 @@
-from classifier import classifier
-from disease import Disease
-
-cold = Disease('cold', ['nausea', 'mild fever', 'headache', 'vomiting', 'body ache', 'throat ache', 'cough'], 9000, 4000, 500)
-typhoid = Disease('typhoid', ['nausea', 'rash', 'weakness', 'abdominal pain', 'high fever', 'headache', 'constipation',
-                              'confusion', 'diarrhoea','vomiting'], 7000, 5000, 4693)
-flu = Disease('flu', ['nausea', 'headache', 'fatigue','body ache', 'cough','sore throat','fever', 'gastrointestinal disorder'], 1000, 800, 200)
-diseases = [cold, typhoid, flu]
-classifier(diseases, ['throat ache', 'mid fever'])
